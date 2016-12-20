cef467a23fe5671e20aa2c5e614f7daf8d6e3484
AhiShahar <ahishahar1987@gmail.com>
2016-12-20 18:36:54 +1100

    finally

--- assets/javascripts/jungle.js
+++ assets/javascripts/jungle.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    // $("a-box").on("click", function() {
-    //     getMovieName($(this).attr("id"));
-    // });
-    // $("a-cylinder").on("click", function() {
-    //     getMovieName($(this).attr("id"));
-    // });
-    // $("#box").on("click", function() {
-    //     $("#box").attr("textwrap").text = "bye";
-    //     console.log(getMovieName($(this).attr("id")));
-    // });
-
-    $(window).on("keypress", function(e) {
-        console.log(e);
-    });
 
     var video = document.querySelector("video");
     video.pause();
--- assets/javascripts/ocean.js
+++ assets/javascripts/ocean.js
@@ -3,5 +3,23 @@ $(document).ready(function() {
     var video = document.querySelector("video");
     video.pause();
 
+    var showVideo = document.querySelector("a-video");
+    showVideo.removeFromParent();
+
+    // document.querySelector('.dolphin').addEventListener('click', function () {
+    //   this.setAttribute('material', 'color', 'red');
+    //
+    //   console.log('I was clicked!');
+    // });
+
+    $(".dolphin").on("click", function () {
+        this.setAttribute('material', 'color', 'red');
+    });
+
+    $(".penguin").on("click", function () {
+        var scene = document.querySelector("a-scene");
+        video.addToParent(scene);
+    });
+
 
 });
