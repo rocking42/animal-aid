188654d3ab0ec07d4c8bc6bb59f3a1be010a061e
rocking42 <nedpike3@gmail.com>
2016-12-20 14:50:22 +1100

    added checkpoints

--- assets/javascripts/jungle.js
+++ assets/javascripts/jungle.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    $("a-box").on("click", function() {
-        getMovieName($(this).attr("id"));
-    });
-    $("a-cylinder").on("click", function() {
-        getMovieName($(this).attr("id"));
-    });
-    $("#box").on("click", function() {
-        $("#box").attr("textwrap").text = "bye";
-        console.log(getMovieName($(this).attr("id")));
+    // $("a-box").on("click", function() {
+    //     getMovieName($(this).attr("id"));
+    // });
+    // $("a-cylinder").on("click", function() {
+    //     getMovieName($(this).attr("id"));
+    // });
+    // $("#box").on("click", function() {
+    //     $("#box").attr("textwrap").text = "bye";
+    //     console.log(getMovieName($(this).attr("id")));
+    // });
+
+    $(window).on("keypress", function(e) {
+        console.log(e);
     });
 
     var video = document.querySelector("video");
