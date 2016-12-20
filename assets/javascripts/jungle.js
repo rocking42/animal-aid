9e471dd03d75b50c959ea1afb4ff1121ecc30138
rocking42 <nedpike3@gmail.com>
2016-12-20 18:37:01 +1100

    added click events

--- assets/javascripts/jungle.js
+++ assets/javascripts/jungle.js
@@ -14,15 +14,24 @@ $(document).ready(function() {
         console.log(e);
     });
 
+    var avideo = document.querySelector("a-video");
+    // avideo.removeFromParent();
     var video = document.querySelector("video");
     video.pause();
 
+
+    //
     var scene = document.querySelector("a-scene");
+    $("#rhinoBox").on("click", function() {
+        video.addToParent(scene);
+    });
+
     var myInterval = window.setInterval(function() {
         if (scene.hasLoaded) {
             console.log("happy");
             var ocean = document.querySelector("a-ocean");
             ocean.pause();
+            avideo.removeFromParent();
             clearInterval(myInterval);
         }
     }, 1000);
