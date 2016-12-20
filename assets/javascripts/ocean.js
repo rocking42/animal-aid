8a4e59c7283338c5c6c8845a6858899a6b94f2cd
AhiShahar <ahishahar1987@gmail.com>
2016-12-21 09:59:20 +1100

    playing with a turtle

--- assets/javascripts/ocean.js
+++ assets/javascripts/ocean.js
@@ -1,25 +1,57 @@
+var $playOrca;
 $(document).ready(function() {
 
-    var video = document.querySelector("video");
-    video.pause();
+    freezeVideos();
 
-    var showVideo = document.querySelector("a-video");
-    showVideo.removeFromParent();
 
-    // document.querySelector('.dolphin').addEventListener('click', function () {
-    //   this.setAttribute('material', 'color', 'red');
-    //
-    //   console.log('I was clicked!');
-    // });
+    var scene = document.querySelector("a-scene");
+    var showPenguin = document.querySelector("#showPenguin");
+    var playPenguin = document.querySelector("#playPenguin");
+    var showDolphin = document.querySelector("#showDolphin");
+    var playDolphin = document.querySelector("#playDolphin");
+    var showOrca = document.querySelector("#showOrca");
+    var playOrca = document.querySelector("#playOrca");
 
     $(".dolphin").on("click", function () {
-        this.setAttribute('material', 'color', 'red');
+        freezeVideos();
+        showDolphin.addToParent("scene");
+        playDolphin.addToParent("scene");
+        var $playDolphinImage = $("#playDolphin").find("a-image");
+        $playDolphinImage.trigger("click");
     });
 
     $(".penguin").on("click", function () {
-        var scene = document.querySelector("a-scene");
-        video.addToParent(scene);
+        freezeVideos();
+        showPenguin.addToParent("scene");
+        playPenguin.addToParent("scene");
+        var $playPenguinImage = $("#playPenguin").find("a-image");
+        $playPenguinImage.trigger("click");
     });
 
+    $(".orcaWhale").on("click", function () {
+        freezeVideos();
+        showOrca.addToParent("scene");
+        playOrca.addToParent("scene");
+        var $playOrcaImage = $("#playOrca").find("a-image");
+        $playOrcaImage.trigger("click");
+    });
 
+    var myInterval = window.setInterval(function() {
+        if (scene.hasLoaded) {
+            showPenguin.removeFromParent();
+            playPenguin.removeFromParent();
+            playDolphin.removeFromParent();
+            showDolphin.removeFromParent();
+            playOrca.removeFromParent();
+            showOrca.removeFromParent();
+            clearInterval(myInterval);
+        }
+    }, 500);
 });
+
+var freezeVideos = function(){
+    var videos = document.querySelectorAll("video");
+    for (var i = 0 ; i < videos.length ; i += 1) {
+        videos[i].pause();
+    }
+};
