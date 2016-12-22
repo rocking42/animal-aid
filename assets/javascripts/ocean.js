59c746b26dbb503ab8a6ba8025da1721aa93ab89
AhiShahar <ahishahar1987@gmail.com>
2016-12-22 11:00:06 +1100

    clean js file

--- assets/javascripts/ocean.js
+++ assets/javascripts/ocean.js
@@ -1,10 +1,3 @@
-// Follows if each video is playing or not
-var isPenguinOn = true;
-var isOrcaOn = true;
-var isDolphinOn = true;
-var isSharkOn = true;
-
-
 // Iterates through all videos and pause them (cant override autoplay)
 var freezeVideos = function(){
     var videos = document.querySelectorAll("video");
@@ -29,59 +22,77 @@ var goJungling = function(){
     }
 };
 
-//  THE NEXT 4 :
-    // remove the video and controllers from site and reset the triggers
-
-var deleteDolphin = function () {
-    showDolphin.removeFromParent();
-    playDolphin.removeFromParent();
-    isDolphinOn = false;
-};
-
-var deleteShark = function () {
-    showShark.removeFromParent();
-    playShark.removeFromParent();
-    isSharkOn = false;
-};
-
-var deleteOrca = function() {
-    showOrca.removeFromParent();
-    playOrca.removeFromParent();
-    isOrcaOn = false;
-};
-
-var deletePenguin = function() {
-    playPenguin.removeFromParent();
-    showPenguin.removeFromParent();
-    isPenguinOn = false;
-};
-
-
 $(document).ready(function() {
 
     freezeVideos();
 
     // once the scene has loaded find all videos, controllers and scene and save them into variables
     var scene = document.querySelector("a-scene");
-    var showPenguin = document.querySelector("#showPenguin");
-    var playPenguin = document.querySelector("#playPenguin");
-    var showDolphin = document.querySelector("#showDolphin");
-    var playDolphin = document.querySelector("#playDolphin");
-    var showOrca = document.querySelector("#showOrca");
-    var playOrca = document.querySelector("#playOrca");
-    var showShark = document.querySelector("#showShark");
-    var playShark = document.querySelector("#playShark");
     var userCam = document.querySelector("#userCam");
 
+    var animals = {
+        orca: {
+            show: document.querySelector("#showOrca"),
+            play: document.querySelector("#playOrca"),
+            isOn: true,
+            name: "playOrca"
+        },
+        dolphin: {
+            show: document.querySelector("#showDolphin"),
+            play: document.querySelector("#playDolphin"),
+            isOn: true,
+            name: "playDolphin"
+        },
+        penguin: {
+            show: document.querySelector("#showPenguin"),
+            play: document.querySelector("#playPenguin"),
+            isOn: true,
+            name: "playPenguin"
+        },
+        shark: {
+            show: document.querySelector("#showShark"),
+            play: document.querySelector("#playShark"),
+            isOn: true,
+            name: "playShark"
+        }
 
+    };
 
-    $(".dolphin").on("click", playDolphinEvent );
+    var removeAnimal = function(animal) {
+            animal["show"].removeFromParent();
+            animal["play"].removeFromParent();
+            animal["isOn"] = false;
+    };
 
-    $(".penguin").on("click", playPenguinEvent );
+    var playEvent = function (event) {
+        freezeVideos();
+        console.log("going to playEvent");
+        for (var key in animals) {
+            console.log("entering the loop");
+            var animal = animals[key];
+            if (event.target.classList.contains(animal.name) && animal.isOn === false) {
+                animal["show"].addToParent("scene");
+                animal["play"].addToParent("scene");
+                var $playVideo = $("#"+animal.name).find("a-image");
+                console.log("Awdawd");
+                console.log($playVideo);
+                console.log("awdqeweqeqwdawd");
+                $playVideo.trigger("click");
+                animal.isOn = true;
+            } else if (animal.isOn ){
+                console.log("cant read condition!!!");
+                removeAnimal(animal);
+            }
+        }
+    };
 
-    $(".orcaWhale").on("click", playOrcaEvent );
+    $(".playDolphin").on("click", playEvent );
 
-    $(".shark").on("click", playSharkEvent );
+    $(".playPenguin").on("click", playEvent );
+
+    $(".playOrca").on("click", playEvent );
+
+    $(".playShark").on("click", playEvent );
 
     $(".goHome").on("click", function() {
         var floor = document.querySelector(".floor");
@@ -101,103 +112,14 @@ $(document).ready(function() {
     // after saving them into variable sand loading the assets, delete the nodes off of the scene
     var myInterval = window.setInterval(function() {
         if (scene.hasLoaded) {
-            deletePenguin();
-            deleteOrca();
-            deleteDolphin();
-            deleteShark();
+            for (var key in animals) {
+                removeAnimal(animals[key]);
+            }
             clearInterval(myInterval);
         }
     }, 500);
+
+    //  EVENT LISTENERS FOR PLATFORMS
+        // If selected video is playing and clicked again, shut it down
+            // OTHERWISE get rid of the rest of the videos on scene and launch the current video
 });
-
-//  EVENT LISTENERS FOR PLATFORMS
-    // If selected video is playing and clicked again, shut it down
-        // OTHERWISE get rid of the rest of the videos on scene and launch the current video
-
-var playPenguinEvent = function (event) {
-    freezeVideos();
-    if (isPenguinOn === true) { // If penguin is playing and clicked again, shut it down
-        deletePenguin();
-    } else { // OTHERWISE get rid of the rest of the videos on scene and launch the penguin video
-        if (isOrcaOn === true) {
-            deleteOrca();
-        }
-        if (isDolphinOn === true) {
-            deleteDolphin();
-        }
-        if (isSharkOn === true) {
-            deleteShark();
-        }
-        showPenguin.addToParent("scene");
-        playPenguin.addToParent("scene");
-        var $playPenguinImage = $("#playPenguin").find("a-image");
-        $playPenguinImage.trigger("click");
-        isPenguinOn = true;
-    }
-};
-
-var playDolphinEvent = function (event) {
-    freezeVideos();
-    if (isDolphinOn === true) {
-        deleteDolphin();
-    } else {
-        if (isPenguinOn === true) {
-            deletePenguin();
-        }
-        if (isOrcaOn === true) {
-            deleteOrca();
-        }
-        if (isSharkOn === true) {
-            deleteShark();
-        }
-        showDolphin.addToParent("scene");
-        playDolphin.addToParent("scene");
-        var $playDolphinImage = $("#playDolphin").find("a-image");
-        $playDolphinImage.trigger("click");
-        isDolphinOn = true;
-    }
-};
-
-var playOrcaEvent = function (event) {
-    freezeVideos();
-    if (isOrcaOn === true) {
-        deleteOrca();
-    } else {
-        if (isPenguinOn === true) {
-            deletePenguin();
-        }
-        if (isDolphinOn === true) {
-            deleteDolphin();
-        }
-        if (isSharkOn === true) {
-            deleteShark();
-        }
-        showOrca.addToParent("scene");
-        playOrca.addToParent("scene");
-        var $playOrcaImage = $("#playOrca").find("a-image");
-        $playOrcaImage.trigger("click");
-        isOrcaOn = true;
-    }
-};
-
-var playSharkEvent = function (event) {
-    freezeVideos();
-    if (isSharkOn === true) {
-        deleteShark();
-    } else {
-        if (isPenguinOn === true) {
-            deletePenguin();
-        }
-        if (isDolphinOn === true) {
-            deleteDolphin();
-        }
-        if (isOrcaOn === true) {
-            deleteOrca();
-        }
-        showShark.addToParent("scene");
-        playShark.addToParent("scene");
-        var $playSharkImage = $("#playShark").find("a-image");
-        $playSharkImage.trigger("click");
-        isSharkOn = true;
-    }
-};
