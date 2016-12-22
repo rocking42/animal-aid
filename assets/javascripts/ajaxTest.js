2c3db67c526165dd8826b980ee996c9c9b24ce39
rocking42 <nedpike3@gmail.com>
2016-12-22 13:30:31 +1100

    AJAX modal logic done

--- assets/javascripts/ajaxTest.js
+++ assets/javascripts/ajaxTest.js
@@ -1,23 +1,45 @@
-function displayAnimal(data) {
-  $(".container").append(JSON.stringify(data));
-}
-
 var locURL = "https://animal-data.herokuapp.com/animals.json";
 
-var findAnimal = function() {
+function modalDisplay(res) {
+  var template = $("#modal1").html();
+  var result = _.template(template);
+  result = result({name: res.name, description: res.description, vidURL: res.vidURL});
+  $(".container").html(result);
+
+  var modal = document.querySelector('#myModal');
+  modal.style.display = "block";
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  };
+}
+
+function displayAnimal(data, b,c, query) {
+  res = data.find(function(animal) {
+    return animal.name === query;
+  });
+  modalDisplay(res);
+}
+
+var findAnimal = function(query) {
   $.ajax({
     url: locURL,
     type: "GET",
     crossDomain: true,
     dataType: "JSON"
-  }).done(displayAnimal)
-    .fail(function( jqXHR, textStatus ) {
+  })
+  .done(function(a,b,c) {
+    displayAnimal(a,b,c, query);
+  })
+  .fail(function( jqXHR, textStatus ) {
       alert( "Request failed: " + textStatus );
-    });
+  });
 };
 
 $(document).ready(function() {
   $("#req").on("click", function() {
-    findAnimal();
+    findAnimal(this.innerHTML);
   });
+
 });
